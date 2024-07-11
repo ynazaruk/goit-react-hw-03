99962de1bc56273a7a4edcabd78375bebfd9e9df
ynazaruk <nazaruk.ya@gmail.com>
2024-07-12 00:17:32 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from "react"
-import contacts from "../../contacts.json"
+import { useState, useEffect } from "react"
+// import contacts from "../../contacts.json"
 import ContactList from "../ContactList/ContactList"
 import css from "./App.module.css"
 import SearchBox from "../SearchBox/SearchBox"
 import ContactForm from "../ContactForm/ContactForm"
 
 
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export default function App() {
-  const [contacts, setContacts]= useState(() => {
-    const storedContacts = JSON.parse(localStorage.getItem("contacts"))
-    return storedContacts || [];
-  } )
+  const [contacts, setContacts]= useState(initialContacts);  
+  
+  useEffect(() => {
+    const storedContacts = localStorage.getItem("contacts")
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    } else {
+      setContacts(initialContacts);
+      localStorage.setItem('contacts', JSON.stringify(initialContacts));
+    }
+  }, []); 
 
   const [searchTerm, setSearchTerm] = useState("");
 
